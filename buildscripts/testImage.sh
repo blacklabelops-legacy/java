1c9b99407cb0680f453cba4afc4bad2043b439e9
Steffen Bleul <sbl@itbleul.de>
2016-02-23 17:27:08 +0100

    Added build JDK8.

--- buildscripts/testImage.sh
+++ buildscripts/testImage.sh
@@ -4,8 +4,11 @@ set -o errexit    # abort script at first error
 
 function testImage() {
   local tagname=$1
+  local distribution=$2
   docker run blacklabelops/java:$tagname java -version
-  docker run blacklabelops/java:$tagname javac -version
+  if [ "${distribution}" = "jdk" ]; then
+    docker run blacklabelops/java:$tagname javac -version
+  fi
   docker run blacklabelops/java:$tagname which java
   docker run blacklabelops/java:$tagname sh -c 'echo $JAVA_HOME'
 }
--- buildscripts/testSupportedImages.sh
+++ buildscripts/testSupportedImages.sh
@@ -8,7 +8,7 @@ readonly CUR_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 printf '%b\n' ":: Reading release config...."
 source $CUR_DIR/release.sh
 
-source $CUR_DIR/testImage.sh centos.jdk6
-source $CUR_DIR/testImage.sh centos.jdk6.$ORACLE_6_JAVA_MAJOR_VERSION.$ORACLE_6_JAVA_UPDATE_VERSION.$ORACLE_6_JAVA_BUILD_NUMBER
-source $CUR_DIR/testImage.sh jdk6
-source $CUR_DIR/testImage.sh jdk6.$ORACLE_6_JAVA_MAJOR_VERSION.$ORACLE_6_JAVA_UPDATE_VERSION.$ORACLE_6_JAVA_BUILD_NUMBER
+source $CUR_DIR/testImage.sh centos.jdk6 jdk
+source $CUR_DIR/testImage.sh centos.jdk6.$ORACLE_6_JAVA_UPDATE_VERSION.$ORACLE_6_JAVA_BUILD_NUMBER jdk
+source $CUR_DIR/testImage.sh jdk6 jdk
+source $CUR_DIR/testImage.sh jdk6.$ORACLE_6_JAVA_UPDATE_VERSION.$ORACLE_6_JAVA_BUILD_NUMBER jdk
