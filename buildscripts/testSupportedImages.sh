b3642117e9b7937c75588fe9c847735bb4236f32
Steffen Bleul <sbl@itbleul.de>
2016-02-23 17:32:11 +0100

    Fixed build number.

--- buildscripts/testSupportedImages.sh
+++ buildscripts/testSupportedImages.sh
@@ -8,6 +8,13 @@ readonly CUR_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 printf '%b\n' ":: Reading release config...."
 source $CUR_DIR/release.sh
 
+# JDK 8
+source $CUR_DIR/testImage.sh centos.jdk8 jdk
+source $CUR_DIR/testImage.sh centos.jdk8.$ORACLE_8_JAVA_UPDATE_VERSION.$ORACLE_8_JAVA_BUILD_NUMBER jdk
+source $CUR_DIR/testImage.sh jdk8 jdk
+source $CUR_DIR/testImage.sh jdk8.$ORACLE_8_JAVA_UPDATE_VERSION.$ORACLE_8_JAVA_BUILD_NUMBER jdk
+
+# JDK 6
 source $CUR_DIR/testImage.sh centos.jdk6 jdk
 source $CUR_DIR/testImage.sh centos.jdk6.$ORACLE_6_JAVA_UPDATE_VERSION.$ORACLE_6_JAVA_BUILD_NUMBER jdk
 source $CUR_DIR/testImage.sh jdk6 jdk
