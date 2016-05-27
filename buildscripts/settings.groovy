a98db39de387cdf3fe28c23138c8582ae8edc1c5
Steffen Bleul <blacklabelops@itbleul.de>
2016-05-27 10:06:11 +0200

    Syntax correction

--- buildscripts/settings.groovy
+++ buildscripts/settings.groovy
@@ -17,24 +17,24 @@ dockerTestCommandsJRE =
   "sh -c \'echo $JAVA_HOME\'"] as String[]
 dockerRepositories = [["","Dockerhub","DockerhubEmail"]] as String[][]
 
-ORACLE_6_JAVA_MAJOR_VERSION = 6
-ORACLE_6_JAVA_UPDATE_VERSION = 45
-ORACLE_6_JAVA_BUILD_NUMBER = 06
+oracle_6_java_major_version = 6
+oracle_6_java_update_version = 45
+oracle_6_java_build_number = 06
 
-ORACLE_7_JAVA_MAJOR_VERSION = 7
-ORACLE_7_JAVA_UPDATE_VERSION = 80
-ORACLE_7_JAVA_BUILD_NUMBER = 15
+oracle_7_java_major_version = 7
+oracle_7_java_update_version = 80
+oracle_7_java_build_number = 15
 
-ORACLE_8_JAVA_MAJOR_VERSION = 8
-ORACLE_8_JAVA_UPDATE_VERSION = 92
-ORACLE_8_JAVA_BUILD_NUMBER = 14
+oracle_8_java_major_version = 8
+oracle_8_java_update_version = 92
+oracle_8_java_build_number = 14
 
-OPENJDK_8_JAVA_MAJOR_VERSION = 8
-OPENJDK_8_JAVA_UPDATE_VERSION = 92
-OPENJDK_8_JAVA_BUILD_NUMBER = 14-r0
+openjdk_8_java_major_version = 8
+openjdk_8_java_update_version = 92
+openjdk_8_java_build_number = 14-r0
 
-OPENJDK_7_JAVA_MAJOR_VERSION = 7
-OPENJDK_7_JAVA_UPDATE_VERSION = 91
-OPENJDK_7_JAVA_BUILD_NUMBER = 2.6.3-r1
+openjdk_7_java_major_version = 7
+openjdk_7_java_update_version = 91
+openjdk_7_java_build_number = 2.6.3-r1
 
 return this;
