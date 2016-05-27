45e906436ca21a3bba08d29f3be37e49a6689984
Steffen Bleul <blacklabelops@itbleul.de>
2016-05-27 15:11:51 +0200

    Adding ubuntu build.

---
+++ java-oracle-ubuntu/settings.groovy
@@ -0,0 +1,32 @@
+/**
+ * Jenkins 2.0 Buildfile
+ **/
+
+imageArgs = [
+    [ "ubuntu.jdk",
+      "jdk",
+      settings.oracle_8_java_major_version,
+      settings.oracle_8_java_update_version,
+      settings.oracle_8_java_build_number],
+    [ "ubuntu.jre",
+      "jre",
+      settings.oracle_8_java_major_version,
+      settings.oracle_8_java_update_version,
+      settings.oracle_8_java_build_number],
+    [ "ubuntu.jdk",
+      "jdk",
+      settings.oracle_7_java_major_version,
+      settings.oracle_7_java_update_version,
+      settings.oracle_7_java_build_number],
+    [ "ubuntu.jre",
+      "jre",
+      settings.oracle_7_java_major_version,
+      settings.oracle_7_java_update_version,
+      settings.oracle_7_java_build_number]
+  ] as String[][]
+
+dockerImages = ["blacklabelops/ubuntu:16.04"] as String[]
+
+dockerWorkspace = "java-oracle-ubuntu"
+
+return this;
