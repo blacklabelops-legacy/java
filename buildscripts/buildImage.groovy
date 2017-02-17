eebd2423d8e912b121a45c19f7c1b5a9887519e3
Steffen Bleul <sbl@itbleul.de>
2017-02-17 22:45:54 +0100

    Fixing build system.

--- buildscripts/buildImage.groovy
+++ buildscripts/buildImage.groovy
@@ -89,7 +89,7 @@ def buildImage(dockerWorkspace, imageName, tagName, branchName, buildargs) {
   if (buildargs.length > 5)
     sh 'cd ' + dockerWorkspace + ' && docker build --no-cache --build-arg JAVA_DISTRIBUTION=' + buildargs[1] + ' --build-arg JAVA_MAJOR_VERSION=' + buildargs[2] + ' --build-arg JAVA_UPDATE_VERSION=' + buildargs[3] + ' --build-arg JAVA_BUILD_NUMBER=' + buildargs[4] + ' --build-arg JAVA_HASH=' + buildargs[5] + ' -t ' + image + ' .'
   else
-    sh 'cd ' + dockerWorkspace + ' && docker build --no-cache --build-arg JAVA_DISTRIBUTION=' + buildargs[1] + ' --build-arg JAVA_MAJOR_VERSION=' + buildargs[2] + ' --build-arg JAVA_UPDATE_VERSION=' + buildargs[3] + ' --build-arg JAVA_BUILD_NUMBER=' + buildargs[4] + ' -t ' + image + ' .'
+    sh 'cd ' + dockerWorkspace + ' && docker build --no-cache --build-arg JAVA_DISTRIBUTION=' + buildargs[1] + ' --build-arg JAVA_MAJOR_VERSION=' + buildargs[2] + ' --build-arg JAVA_UPDATE_VERSION=' + buildargs[3] + ' --build-arg JAVA_BUILD_NUMBER=' + buildargs[4] + ' --build-arg JAVA_HASH=' + -t ' + image + ' .'
 }
 
 return this;
